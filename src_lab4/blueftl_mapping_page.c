b4b4836b1191e378569579c5c66bededdabace69
mattjs17 <jhshim96@naver.com>
2019-05-07 18:42:33 +0900

    Remove comment out part from logical_page_mapping

--- src_lab4/blueftl_mapping_page.c
+++ src_lab4/blueftl_mapping_page.c
@@ -339,51 +339,7 @@ int32_t map_logical_to_physical(struct ftl_context_t *ptr_ftl_context, uint32_t 
 		exit(1);
 		return -1;
 	}
-	// (2) change the status of the previous block.
-	// get the previous physical page address from the page mapping table
-	// previous_physical_page_address = ptr_mapping_table->ptr_pg_table[logical_page_address];
-
-	// make the previous page invalid
-	// if (previous_physical_page_address != PAGE_TABLE_FREE)
-	// {
-	// 	/* get the physical layout from the physical page address */
-	// 	ftl_convert_to_ssd_layout(previous_physical_page_address, &curr_bus, &curr_chip, &curr_block, &curr_page);
-
-	// 	/* get the erase block from the block mapping table */
-	// 	ptr_erase_block = &(ptr_ssd->list_buses[curr_bus].list_chips[curr_chip].list_blocks[curr_block]);
-
-	// 	/* update the last modified time */
-	// 	ptr_erase_block->last_modified_time = timer_get_timestamp_in_sec();
-
-	// 	/* increase the number of invalid pages while reducing the number of valid pages */
-	// 	if (ptr_erase_block->nr_valid_pages > 0)
-	// 	{
-	// 	//	ptr_erase_block->nr_invalid_pages++;
-	// 	//	ptr_erase_block->nr_valid_pages--;
-
-	// 		// if (ptr_erase_block->list_pages[curr_page].page_status != PAGE_STATUS_VALID)
-	// 		// {
-	// 		// 	printf("blueftl_mapping_page: the previous page should be a valid status\n");
-	// 		// 	return -1;
-	// 		// }
-
-	// 		ptr_erase_block->list_pages[curr_page].page_status = PAGE_STATUS_INVALID;
-	// 	}
-	// 	else
-	// 	{
-	// 		printf("blueftl_mapping_page: nr_valid_pages is zero\n");
-	// 		return -1;
-	// 	}
-
-	// 	/* see if the number of pages is correct */
-	// 	if ((ptr_erase_block->nr_free_pages + ptr_erase_block->nr_valid_pages + ptr_erase_block->nr_invalid_pages) != ptr_ssd->nr_pages_per_block)
-	// 	{
-	// 		printf("blueftl_mapping_page: free:%u + valid:%u + invalid:%u is not %u\n",
-	// 			   ptr_erase_block->nr_free_pages, ptr_erase_block->nr_valid_pages, ptr_erase_block->nr_invalid_pages, ptr_ssd->nr_pages_per_block);
-	// 		return -1;
-	// 	}
-	// }
-	// (3) change the status of the given block
+	
 	ftl_convert_to_ssd_layout(physical_page_address, &curr_bus, &curr_chip, &curr_block, &curr_page);
 
 	if ((ptr_erase_block = &(ptr_ssd->list_buses[curr_bus].list_chips[curr_chip].list_blocks[curr_block])) == NULL)
@@ -395,16 +351,6 @@ int32_t map_logical_to_physical(struct ftl_context_t *ptr_ftl_context, uint32_t 
 
 	if (ptr_erase_block->nr_free_pages > 0)
 	{
-		// ptr_erase_block->nr_valid_pages++;
-		// ptr_erase_block->nr_free_pages--;
-
-		/* in memory compression -> ppa 겹칠 수 있음 */
-		// if (ptr_erase_block->list_pages[curr_page].page_status != PAGE_STATUS_FREE)
-		// {
-		// 	// the given page should be free
-		// 	printf("blueftl_mapping_page: the given page should be free (%u) (%u)\n", ptr_erase_block->list_pages[curr_page].page_status, curr_page);
-		// 	return -1;
-		// }
 
 		ptr_erase_block->list_pages[curr_page].page_status = PAGE_STATUS_VALID;
 		ptr_erase_block->list_pages[curr_page].no_logical_page_addr = logical_page_address;
@@ -416,15 +362,6 @@ int32_t map_logical_to_physical(struct ftl_context_t *ptr_ftl_context, uint32_t 
 		printf("blueftl_mapping_page: nr_free_page is zero before writing the data into flash memory\n");
 		return -1;
 	}
-
-
-	// CHECK: see if the sum of different types of pages is the same as the number of pages in a block
-	// if ((ptr_erase_block->nr_free_pages + ptr_erase_block->nr_valid_pages + ptr_erase_block->nr_invalid_pages) != ptr_ssd->nr_pages_per_block)
-	// {
-	// 	printf("blueftl_mapping_page: free:%u + valid:%u + invalid:%u is not %u\n",
-	// 		   ptr_erase_block->nr_free_pages, ptr_erase_block->nr_valid_pages, ptr_erase_block->nr_invalid_pages, ptr_ssd->nr_pages_per_block);
-	// 	return -1;
-	// }
 
 	// (4) map the given logical page address to the physical page address
 	ptr_mapping_table->ptr_pg_table[logical_page_address] = physical_page_address;
